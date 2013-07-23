d9214451a7448098de1d88500c37eca97f4fafff
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2013-07-24 00:41:16 +0200

    Also generate source code map of app.coffee

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-coffee --watch --compile --bare --output ./static/lib ./static/lib/app.coffee &
+coffee --watch --compile --bare --map --output ./static/lib ./static/lib/app.coffee &
 
 /usr/local/bin/python2.7 ./server.py
